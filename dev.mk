201d510621ac35403a49bef18b65c5a3fd7f41cd
Alfonso Ruzafa <alfonso.ruzafa@softonic.com>
2017-09-26 15:56:06 +0200

    Added image build

--- dev.mk
+++ dev.mk
@@ -1,16 +1,30 @@
+SHELL := /bin/bash
 CURL := curl
 PHP := php
+TEX2PDF := pdflatex
+TEX2PDFFLAGS := -halt-on-error
+PDF2PNG := convert
+PDF2PNGFLAGS := -density 200 -quality 10
 
 DATAFILES := $(wildcard src/data/*)
 VIEWFILES := $(wildcard src/views/*)
-HTMLFILES := index.html index-es.html
-
-TARGET := $(addprefix gh-pages/,$(HTMLFILES))
+TEXSHAREDFILES := $(wildcard src/images/_*.tex)
+TEXFILES := $(filter-out $(TEXSHAREDFILES),$(wildcard src/images/*))
+PNGFILES := $(patsubst src/images/%.tex,gh-pages/images/%.png,$(TEXFILES))
+HTMLFILES := $(addprefix gh-pages/,index.html index-es.html)
+TARGET := $(HTMLFILES) $(PNGFILES)
 
 all: $(TARGET)
 
 gh-pages/%.html: src/build-index.php $(VIEWFILES) $(DATAFILES)
 	$(PHP) src/build-index.php $@
+
+gh-pages/%.png: src/%.tex $(TEXSHAREDFILES)
+	pushd $(<D) && \
+	$(TEX2PDF) $(TEX2PDFFLAGS) -output-directory=/tmp -jobname=image $(<F) && \
+	popd
+	mkdir -p $(@D)
+	$(PDF2PNG) $(PDF2PNGFLAGS) /tmp/image.pdf $@
 
 src/build-index.php: vendor/autoload.php
 
